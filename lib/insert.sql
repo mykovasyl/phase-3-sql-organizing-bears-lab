034936ba5ff42547601e96bb03f1a29381eca9ee
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-07-12 19:20:06 -0400

    commit tree badfc49714086960fe349de79110cfc8605f3599

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+("Mr. Chocolate", 37, "M", "Brown", "Calm", True),
+("Rowdy", 25, "F", "Black", "Angry", True),
+("Tabitha", 12, "F", "White", "Dreamy", True),
+("Sergeant Brown", 69, "M", "Red", "Strict", True),
+("Melissa", 46, "F", "Orange", "Carefree", True),
+("Grinch", 2, "M", "Green", "Sneaky", True),
+("Wendy", 5, "F", "Red", "Curious", True),
+(NULL, 10, "M", "Pink", "Shy", True)
